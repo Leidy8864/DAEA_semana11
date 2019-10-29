9942d7b552d69e4b2d16f1202f44e574a6627725
leidy8864 <leidy.callupe@tecsup.edu.pe>
2019-10-28 19:35:41 -0500

    Agregar archivos de proyecto.

---
+++ MVCAJAX/Models/StudentModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCAJAX.Controllers
+{
+    public class StudentModel
+    {
+        public int ID { get; set; }
+        public string Address { get; set; }
+        public string Name { get; set; }
+    }
+}
